3bb4e773fef515ed553d1a428058dfd8204078a5
mitarcon <mitarcon@gmail.com>
2017-04-15 12:25:58 -0500

    Recibiendo parametros por URL

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService,  Heroe } from '../../services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -8,15 +9,20 @@ import { HeroesService,  Heroe } from '../../services/heroes.service';
 export class HeroesComponent implements OnInit {
 
   private heroes:Heroe[] = [];
-  constructor( private _heroesService:HeroesService ) {
-    console.log("constructor HeroesComponent");
+  constructor( private _heroesService:HeroesService,
+              private _router:Router
+              ) {
+    // console.log("constructor HeroesComponent");
   }
 
   ngOnInit() {
-    console.log("onInit HeroesComponent");
+    // console.log("onInit HeroesComponent");
     this.heroes = this._heroesService.getHeroes();
-    console.log( this.heroes );
+    // console.log( this.heroes );
+  }
 
+  selectHeroe(idx:number){
+    this._router.navigate( ['/heroe',idx] );
   }
 
 }
