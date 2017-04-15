4982c27a8ee9b275cda3a086d4544e737bf3a6c5
mitarcon <mitarcon@gmail.com>
2017-04-14 23:04:24 -0500

    Creado el servicio de Heroes

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService,  Heroe } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
@@ -6,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeroesComponent implements OnInit {
 
-  constructor() { }
+  private heroes:Heroe[] = [];
+  constructor( private _heroesService:HeroesService ) {
+    console.log("constructor HeroesComponent");
+  }
 
   ngOnInit() {
+    console.log("onInit HeroesComponent");
+    this.heroes = this._heroesService.getHeroes();
+    console.log( this.heroes );
+
   }
 
 }
